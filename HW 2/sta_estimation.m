b63dafa68d6fe1c3285114305151f3a386ddc96f
Schmill731 <williamschmitt@college.harvard.edu>
2018-10-14 16:55:43 -0400

    half-finished attempt of 1f

--- HW 2/sta_estimation.m
+++ HW 2/sta_estimation.m
@@ -13,10 +13,26 @@ sampling_rate = mean(median(diff(stim_time)));
 sta_time = (-t_past/1000):sampling_rate:(t_future/1000);
 sta_freq = stim_freq;
 
+freq_of_interest = zeros(38, 982);
+for i = 1:982
+    matrix = abs(spikes(i)-stim_time);
+    index = find(matrix == min(matrix));
+    times_of_interest = sta_time + stim_time(index);
+    logical = zeros(1, length(stim_time));
+    for j = 1:51
+        logical = logical + (stim_time == times_of_interest(j));
+    end
+    freq_of_interest(:, i) = stim_spectrogram(:, logical);
+end
+
+
+% freq_of_interest = stim_spectrogram(:, time_windows(1, :));
+% freq_of_interest = [freq_of_interest(1:25), 0, freq_of_interest(26:50)];
+
 % sta = ???
 
 % Plot results
-figure(2)
-plot_spectrogram(sta, sta_time, sta_freq);
-xlabel('Time relative to spike (ms)')
-colorbar
+% figure(2)
+% plot_spectrogram(sta, sta_time, sta_freq);
+% xlabel('Time relative to spike (ms)')
+% colorbar
