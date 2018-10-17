cf9ef1bdc6b806ba10b43a5c6fc6dba2f7172b9b
Schmill731 <williamschmitt@college.harvard.edu>
2018-10-17 16:04:49 -0400

    finished q3 code

---
+++ HW 2/problem3code/problem3.m
@@ -0,0 +1,69 @@
+%% Part A
+clear all
+load('problem3code/toWhiten.mat')
+
+% Part i
+demeaned = remove_mean(toWhiten);
+mean(demeaned)
+
+% Part ii
+covar_matrix = covariance_matrix(demeaned);
+cov(demeaned);
+
+% Part iii
+[vectors, values] = eig(covar_matrix);
+scatter(demeaned(:, 1), demeaned(:, 2));
+hold on
+plotv([5*vectors, -5*vectors], 'k')
+
+% Part iv
+rand1 = randn(1000, 5000);
+rand2 = randn(5000, 1000);
+
+tic;
+[U1, S1, V1] = svd(rand1);
+svd1 = toc;
+
+tic;
+[U2, S2, V2] = svd(rand2);
+svd2 = toc;
+
+tic;
+covm1 = cov(rand1);
+[U3, V3] = eig(covm1);
+cov1 = toc;
+
+tic;
+covm2 = cov(rand2);
+[U4, V4] = eig(covm2);
+cov2 = toc;
+
+%% Part B
+ouput = whiten_data(toWhiten);
+scatter(ouput(:, 1), ouput(:, 2))
+
+%% Part C
+
+% Plot images
+clear all
+load('problem3code/mixedImg.mat');
+plotImgs(imMix);
+
+% Whiten Images
+whitened_images = whiten_data(imMix);
+plotImgs(whitened_images);
+
+
+% Find separate images
+weights = learnWeights(whitened_images);
+final = whitened_images*weights;
+
+
+%% Part A Functions
+function output = remove_mean(matrix)
+    output = matrix - mean(matrix);
+end
+
+function output = covariance_matrix(matrix)
+    output = (matrix'*matrix)/(size(matrix, 1)-1);
+end
---
+++ HW 2/problem3code/whiten_data.m
@@ -0,0 +1,9 @@
+function output = whiten_data(input)
+    demeaned = input - mean(input);
+    covar = cov(demeaned);
+    [PC, S] = eig(covar);
+    [sigma, sort_indices] = sort(diag(S), 'descend');
+    PC_sorted = PC(:, sort_indices);
+    basis = PC_sorted(:, 1:size(input, 2));
+    output = input*basis;
+end
