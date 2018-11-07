8d7d768fb8228423fa018b67e3d664f5fdc9e505
Schmill731 <williamschmitt@college.harvard.edu>
2018-11-06 23:08:18 -0500

    figured out neural net error in q3 on hw3

--- HW 3/backprop.m
+++ HW 3/backprop.m
@@ -1,4 +1,7 @@
 clear all;
+
+diag=true;
+
 %% Load MNIST dataset and plot some examples
 
 load mnist_sevens_nines
@@ -20,8 +23,8 @@ end
 
 %% Implement gradient descent
 P = size(X_train,2); % Number of examples
-max_itr = 500;
-alpha = .1/P;
+max_itr = 5000;
+alpha = (.1/P);
 
 Ni = size(X_train,1); % Input dimension
 Nh1 = 100; % Hidden layer sizes
@@ -40,27 +43,36 @@ for i = 1:max_itr
     % Compute forward propagation for train and test data
     
     % Train data
-    h1 = max(W1*X_train, 0);
-    h2 = max(W2*h1, 0);
+    u1 = W1*X_train;
+    h1 = max(u1, 0);
+    u2 = W2*h1;
+    h2 = max(u2, 0);
     yh = W3*h2;
     
     % Test data
     yh_test = W3*max(W2*max(W1*X_test, 0), 0);
     
     % Compute losses and accuracy on train and test data
-    L(i) = 1/2*norm(y_train-yh,'fro')^2;
+    L(i) = (1/2)*norm(y_train-yh,'fro')^2;
     acc(i) = mean(sign(yh)==sign(y_train));
     
+    if diag
+        disp(['at epoch ',num2str(i),' loss is ',num2str(L(i))])
+        if L(i)>10^200
+            pause
+        end
+    end
+    
   
-    L_test(i) = 1/2*norm(y_test-yh_test,'fro')^2;
+    L_test(i) = (1/2)*norm(y_test-yh_test,'fro')^2;
     acc_test(i) = mean(sign(yh_test)==sign(y_test));
     
     % Compute backprop (just on training data)
     e = y_train - yh;
 
     d3 = e;
-    d2 = (W3'*d3).*d_relu(h2);
-    d1 = (W2'*d2).*d_relu(h1);
+    d2 = (W3'*d3).*d_relu(u2);
+    d1 = (W2'*d2).*d_relu(u1);
 
     % Compute gradient
     g3 = d3*h2';
@@ -68,16 +80,18 @@ for i = 1:max_itr
     g1 = d1*X_train';
 
     % Update weights
-    W3 = W3 - alpha*g3;
-    W2 = W2 - alpha*g2;
-    W1 = W1 - alpha*g1;
+    W3 = W3 + alpha*g3;
+    W2 = W2 + alpha*g2;
+    W1 = W1 + alpha*g1;
 end
 
 % Plot the learning trajectory
 t = 1:max_itr;
 
 subplot(121)
-plot(t,L/P,t,L_test/size(X_test,2),'linewidth',2) % Plot train and test loss normalized by the number of examples
+plot(t,L/P,t,L_test/size(X_test,2),...
+    'linewidth',2) 
+% Plot train and test loss normalized by the number of examples
 legend('Train loss','Test loss')
 xlabel('Epochs')
 ylabel('MSE')
@@ -88,6 +102,6 @@ legend('Train accuracy','Test accuracy')
 
 function d_res = d_relu(A)
     d_res = A;
-    d_res(d_res > 0) = 1;
-    d_res(d_res <= 0) = 0;
+    d_res(d_res >= 0) = 1;
+    d_res(d_res < 0) = 0;
 end
